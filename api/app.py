b05c38d7d312a4d97dac29e8a3950c4293a6552d
Hugo Rodrigues Dias <rodrigueshugodias@gmail.com>
2025-11-26 21:12:53 -0300

    removed flassger

--- api/app.py
+++ api/app.py
@@ -16,7 +16,6 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-from flasgger import Swagger
 
 from dotenv import load_dotenv
 
@@ -38,7 +37,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
-swagger = Swagger(app)
 
 
 # LOGGING
